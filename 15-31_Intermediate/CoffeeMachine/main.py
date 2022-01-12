157b2a23d8c54144d9edfb643038fbd0c988de15
Yuchen Liu <1070552365@qq.com>
2022-01-12 00:35:55 -0500

    separate data and code

--- 15-31_Intermediate/CoffeeMachine/main.py
+++ 15-31_Intermediate/CoffeeMachine/main.py
@@ -1,36 +1,4 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "milk": 0,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
-
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-    "money": 0
-}
+from data import MENU, resources
 
 
 # TODO: 1. Print report.
