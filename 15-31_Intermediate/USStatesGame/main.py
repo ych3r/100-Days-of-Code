62ece10036e1feea9378134e67c872ad8df7c2cb
Yuchen Liu <1070552365@qq.com>
2022-01-21 09:34:04 -0500

    update US state game

--- 15-31_Intermediate/USStatesGame/main.py
+++ 15-31_Intermediate/USStatesGame/main.py
@@ -14,13 +14,15 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        states_to_learn = []
-        for state in state_list:
-            if state not in guessed_states:
-                states_to_learn.append(state)
+        # states_to_learn = []
+        # for state in state_list:
+        #     if state not in guessed_states:
+        #         states_to_learn.append(state)
+        states_to_learn = [state for state in state_list if state not in guessed_states]
         df = pandas.DataFrame(states_to_learn)
         df.to_csv("States_to_learn.csv")
         break
+
     if answer_state in state_list:
         guessed_states.append(answer_state)
         state_data = data[data["state"] == answer_state]
